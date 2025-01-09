d05d04d506e40bb64da517f1a391c5de7979152c
Piotr Stelmach <piotrstelmach135@gmail.com>
2025-01-09 12:13:39 +0100

    Initial commit

---
+++ apps/frontend-e2e/eslint.config.cjs
@@ -0,0 +1,27 @@
+const playwright = require('eslint-plugin-playwright');
+const baseConfig = require('../../eslint.config.cjs');
+
+module.exports = [
+  playwright.configs['flat/recommended'],
+
+  playwright.configs['flat/recommended'],
+
+  playwright.configs['flat/recommended'],
+
+  ...baseConfig,
+  {
+    files: ['**/*.ts', '**/*.js'],
+    // Override or add rules here
+    rules: {},
+  },
+  {
+    files: ['**/*.ts', '**/*.js'],
+    // Override or add rules here
+    rules: {},
+  },
+  {
+    files: ['**/*.ts', '**/*.js'],
+    // Override or add rules here
+    rules: {},
+  },
+];
