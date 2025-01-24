fbadce163cbb2f04ad9202180500b3cbd2d34f09
Piotr Stelmach <piotrstelmach135@gmail.com>
2025-01-24 20:08:55 +0100

    Initial user response caching

--- apps/backend/src/controllers/user.controller.ts
+++ apps/backend/src/controllers/user.controller.ts
@@ -1,5 +1,9 @@
 import { ErrorResponse, TypedRequestBody } from '../types/global';
-import { GiveRoleInput, NewUserInput, UpdateUserInput } from '../types/http/user.http';
+import {
+  GiveRoleInput,
+  NewUserInput,
+  UpdateUserInput,
+} from '../types/http/user.http';
 import { Request, Response } from 'express';
 import {
   changeUserRole,
@@ -7,9 +11,13 @@ import {
   deleteUserById,
   getAllUsers,
   getUserById,
-  updateExistingUser
+  updateExistingUser,
 } from '../services/user.service';
 import { User } from '@prisma/client';
+import { redisClient } from '../utils/redisClient';
+import { mapRedisHash, saveToRedisHash } from '../utils/redisCache';
+
+const USER_CACHE_NAME = 'user';
 
 export class UserController {
   async getUsers(_req: Request, res: Response<User[] | ErrorResponse>) {
@@ -27,13 +35,24 @@ export class UserController {
     if (!req.params?.userId) {
       return res.status(400).json({ error: 'User ID is required' });
     }
-
-    try {
-      const user = await getUserById(Number(req.params.userId));
-      return res.status(200).json(user);
-    } catch (error) {
-      if (error instanceof Error) {
-        return res.status(500).json({ error: error.message });
+    const cache = await redisClient.hGetAll(
+      `${USER_CACHE_NAME}:${req.params?.userId}`
+    );
+    const cachedUser: User = mapRedisHash<User>(cache);
+    if (cachedUser) {
+      return res.status(200).json(cachedUser);
+    } else {
+      try {
+        const user = await getUserById(Number(req.params.userId));
+        await redisClient.hSet(
+          `${USER_CACHE_NAME}:${req.params.userId}`,
+          saveToRedisHash<User>(user)
+        );
+        return res.status(200).json(user);
+      } catch (error) {
+        if (error instanceof Error) {
+          return res.status(500).json({ error: error.message });
+        }
       }
     }
   }
@@ -64,12 +83,15 @@ export class UserController {
       return res.status(400).json({ error: 'User ID is required' });
     }
 
-    if(!req?.body) {
+    if (!req?.body) {
       return res.status(400).json({ error: 'Request body is required' });
     }
 
     try {
-      const user = await updateExistingUser(Number(req.params.userId), req.body);
+      const user = await updateExistingUser(
+        Number(req.params.userId),
+        req.body
+      );
       return res.status(200).json(user);
     } catch (error) {
       if (error instanceof Error) {
@@ -93,7 +115,10 @@ export class UserController {
     }
   }
 
-  async giveUserRole(req: TypedRequestBody<GiveRoleInput>, res: Response<User | ErrorResponse>) {
+  async giveUserRole(
+    req: TypedRequestBody<GiveRoleInput>,
+    res: Response<User | ErrorResponse>
+  ) {
     try {
       if (!req.params?.userId) {
         return res.status(400).json({ error: 'User ID is required' });
@@ -103,7 +128,10 @@ export class UserController {
         return res.status(400).json({ error: 'Roles are required' });
       }
 
-      const updatedUser = await changeUserRole(Number(req.params?.userId), req.body.roles);
+      const updatedUser = await changeUserRole(
+        Number(req.params?.userId),
+        req.body.roles
+      );
       return res.status(200).json(updatedUser);
     } catch (error) {
       if (error instanceof Error) {
