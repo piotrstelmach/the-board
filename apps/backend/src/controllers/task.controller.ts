41fff896ea4df7aa008f924d9b866b9a78c6fe07
Piotr Stelmach <piotrstelmach135@gmail.com>
2025-01-29 21:55:51 +0100

    Tasks pagination

--- apps/backend/src/controllers/task.controller.ts
+++ apps/backend/src/controllers/task.controller.ts
@@ -1,8 +1,17 @@
-import { ErrorResponse, TypedRequestBody } from '../types/global';
+import {
+  ErrorResponse,
+  TypedRequestBody,
+  TypedRequestQueryParams,
+} from '../types/global';
 import { NewTaskInput, UpdateTaskInput } from '../types/http/task.http';
 import { Task } from '@prisma/client';
 import { Request, Response } from 'express';
 import * as taskService from '../services/task.service';
+import { PaginationParams } from '../types/http/pagination.http';
+import {
+  DEFAULT_PAGINATION_LIMIT,
+  DEFAULT_PAGINATION_PAGE,
+} from '../config/pagination';
 
 export class TaskController {
   async createTask(
@@ -23,9 +32,15 @@ export class TaskController {
     }
   }
 
-  async getTasks(_req: Request, res: Response<Task[] | ErrorResponse>) {
+  async getTasks(
+    req: TypedRequestQueryParams<PaginationParams>,
+    res: Response<Task[] | ErrorResponse>
+  ) {
     try {
-      const tasks = await taskService.getAllTasks();
+      const tasks = await taskService.getAllTasks(
+        req.query.page ?? DEFAULT_PAGINATION_PAGE,
+        req.query.limit ?? DEFAULT_PAGINATION_LIMIT
+      );
       return res.status(200).json(tasks);
     } catch (error) {
       if (error instanceof Error) {
@@ -60,7 +75,6 @@ export class TaskController {
     if (!req.body) {
       return res.status(400).json({ error: 'Request body is required' });
     }
-
 
     try {
       const task = await taskService.updateTask(
--- apps/backend/src/routes/task.routes.ts
+++ apps/backend/src/routes/task.routes.ts
@@ -1,13 +1,22 @@
 import { Router } from 'express';
 import { TaskController } from '../controllers/task.controller';
-import { validateRequestInput } from '../middleware/validateRequestInput';
+import {
+  validateRequestInput,
+  validateRequestParams,
+} from '../middleware/validateRequestInput';
 import { NewTaskInput, UpdateTaskInput } from '../types/http/task.http';
 import { checkUserAuthorization } from '../middleware/checkUserAuthorization';
+import { paginationValidator } from '../validators/pagination';
 
 const taskRouter: Router = Router();
 const taskController = new TaskController();
 
-taskRouter.get('/',checkUserAuthorization, taskController.getTasks);
+taskRouter.get(
+  '/',
+  validateRequestParams(paginationValidator),
+  checkUserAuthorization,
+  taskController.getTasks
+);
 taskRouter.get(
   '/:taskId',
   checkUserAuthorization,
@@ -21,6 +30,10 @@ taskRouter.post(
   taskController.createTask
 );
 taskRouter.put('/:taskId', checkUserAuthorization, taskController.updateTask);
-taskRouter.delete('/:taskId', checkUserAuthorization, taskController.deleteTask);
+taskRouter.delete(
+  '/:taskId',
+  checkUserAuthorization,
+  taskController.deleteTask
+);
 
 export { taskRouter as taskRoutes };
