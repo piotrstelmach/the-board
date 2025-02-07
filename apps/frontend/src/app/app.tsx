639e7a9fd2484e40f0f3f59edbdc2a0a3eac029a
Piotr Stelmach <piotrstelmach135@gmail.com>
2025-02-07 22:12:00 +0100

    Initial layout creating

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -2,34 +2,38 @@ import { Link } from 'react-router';
 import { AppRoutes } from './router';
 import { useRefreshToken } from '../hooks/useRefreshToken';
 import { ThemeSwitchButton } from '../components/themeSwitchButton';
+import { Layout } from '../components/layout';
 
 export function App() {
   useRefreshToken();
 
   return (
-    <div>
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/login">login</Link>
-          </li>
-          <li>
-            <Link to="/register">register</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">dashboard</Link>
-          </li>
-        </ul>
-      </div>
-      <ThemeSwitchButton />
-      <AppRoutes />
-    </div>
+    <>
+      <Layout />
+    </>
+    // <div>
+    //   <br />
+    //   <hr />
+    //   <br />
+    //   <div role="navigation">
+    //     <ul>
+    //       <li>
+    //         <Link to="/">Home</Link>
+    //       </li>
+    //       <li>
+    //         <Link to="/login">login</Link>
+    //       </li>
+    //       <li>
+    //         <Link to="/register">register</Link>
+    //       </li>
+    //       <li>
+    //         <Link to="/dashboard">dashboard</Link>
+    //       </li>
+    //     </ul>
+    //   </div>
+    //   <ThemeSwitchButton />
+    //   <AppRoutes />
+    // </div>
   );
 }
 
